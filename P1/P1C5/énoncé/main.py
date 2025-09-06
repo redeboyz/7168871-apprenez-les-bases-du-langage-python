e9de758fd2e26cdc37c84f587bd6b458364f3032
redeboyz <edermusepu@gmail.com>
2025-09-06 01:19:26 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+name = "reDE"
+age = 26
+taille = 1,80
+est_etudiant = True
+
+print (f"Nom: {name}")
+print (f"Age: {age}")
+print (f"Taille: {taille}")
+print (f"Est étudiant: {est_etudiant}")
+
+print (f"Type nom: type (name)")
+print (f"Type age: type (age)")
+print (f"Type taille: type (taille)")
+print (f"Type est étudiant: type (est_etudiant)")
