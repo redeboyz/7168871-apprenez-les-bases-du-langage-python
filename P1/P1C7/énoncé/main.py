29f49081493e4e3831b61516312a435461aa3812
redeboyz <edermusepu@gmail.com>
2025-09-12 00:33:00 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,16 @@
-# Écrivez votre code ici !
+fruits={
+  "pomme":"rouge",
+  "banane":"jaune",
+  "orange":"orange"
+}
+
+fruits ["kiwi"]="verte"
+
+fruits ["banane"]
+couleur_banane = fruits["banane"]
+
+fruits ["pomme"]="vert"
+
+del fruits ["banane"]
+
+print (fruits)
